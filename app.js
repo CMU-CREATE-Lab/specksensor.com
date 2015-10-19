876cb2519b3aff06c9333e8d4f810dbd5c547be8
Chris Bartley <bartley@cmu.edu>
2015-10-19 14:48:37 -0400

    Added press page

--- app.js
+++ app.js
@@ -143,6 +143,23 @@ flow.series([
                                                                               return options.fn(this);
                                                                            }
                                                                            return options.inverse(this);
+                                                                        },
+                                                                        dateToString : function(date) {
+                                                                           if (date) {
+                                                                              var monthNames = [
+                                                                                 "January", "February", "March",
+                                                                                 "April", "May", "June", "July",
+                                                                                 "August", "September", "October",
+                                                                                 "November", "December"
+                                                                              ];
+
+                                                                              var day = date.getDate();
+                                                                              var monthIndex = date.getMonth();
+                                                                              var year = date.getFullYear();
+
+                                                                              return monthNames[monthIndex] + " " + day + ", " + year;
+                                                                           }
+                                                                           return "";
                                                                         }
                                                                      }
                                                                   });
--- routes/index.js
+++ routes/index.js
@@ -133,7 +133,234 @@ router.get('/about-us', function(req, res) {
 });
 
 router.get('/press', function(req, res) {
-   res.render('press', { title : "Press", section : "press" });
+   res.render('press', {
+      title : "Press",
+      section : "press",
+      articles : [
+         {
+            title : "Pike library to lend air-pollution monitors",
+            author : "Jessica Cohen",
+            source : "Times Herald-Record",
+            date : new Date("2015-10-05 00:00:00"),
+            file : {
+               name : "20151005_Times_Herald_Record.pdf",
+               type : "pdf"
+            },
+            url : "http://www.recordonline.com/article/20151005/NEWS/151009639"
+         },
+         {
+            title : "Are cheap sensors, concerned citizens leading to a shift in air monitoring?",
+            author : "Natasha Khan",
+            source : "PublicSource",
+            date : new Date("2015-09-06 00:00:00"),
+            url : "https://timesleader.com/news/380660/are-cheap-sensors-concerned-citizens-leading-to-a-shift-in-air-monitoring"
+         },
+         {
+            title : "Clean Up the Shenango Coke Works",
+            author : "Thaddeus Popovich",
+            source : "Pittsburgh Post Gazette",
+            date : new Date("2015-09-03 00:00:00"),
+            file : {
+               name : "20150903_Pittsburgh_Post_Gazette.pdf",
+               type : "pdf"
+            },
+            url : "http://www.post-gazette.com/opinion/2015/09/03/Clean-up-the-Shenango-coke-works/stories/201509030037"
+         },
+         {
+            title : "What Are We Breathing?",
+            author : "",
+            source : "Heinz Endowments",
+            date : new Date("2015-08-20 00:00:00"),
+            url : "https://www.youtube.com/watch?v=Qgh5Bz57lt0"
+         },
+         {
+            title : "How to Breathe Easy in Polluted Cities",
+            author : "Sarah O'Meara",
+            source : "CityLab",
+            date : new Date("2015-07-14 00:00:00"),
+            file : {
+               name : "20150714_CityLab.pdf",
+               type : "pdf"
+            },
+            url : "http://www.citylab.com/navigator/2015/07/how-to-breathe-easy-in-polluted-cities/398414/"
+         },
+         {
+            title : "A Home Air Quality Monitor That Can Be Checked Out From The Library",
+            author : "Larkin Page-Jacobs",
+            source : "NPR",
+            date : new Date("2015-05-24 00:00:00"),
+            file : {
+               name : "20150524_NPR.mp3",
+               type : "audio"
+            },
+            url : "http://www.npr.org/2015/05/24/408786881/a-home-air-quality-monitor-that-can-be-checked-out-from-the-library"
+         },
+         {
+            title : "Q&amp;A: Laying foundation for cleaner air",
+            author : "Patty Tascarella",
+            source : "Pittsburgh Business Times",
+            date : new Date("2015-05-15 00:00:00"),
+            file : {
+               name : "20150515_PBT.pdf",
+               type : "pdf"
+            },
+            url : "http://www.bizjournals.com/pittsburgh/print-edition/2015/05/15/laying-foundation-for-cleaner-air.html"
+         },
+         {
+            title : "New device from CMU group monitors air quality in homes: Speck measures particulates with air intake",
+            author : "Michelle Wright",
+            source : "Pittsburgh's Channel 4 Action News",
+            date : new Date("2015-05-08 00:00:00"),
+            file : {
+               name : "20150508_Channel4ActionNews.pdf",
+               type : "pdf"
+            },
+            url : "http://www.wtae.com/news/new-device-speck-from-cmu-create-lab-monitors-air-quality-in-homes/32897792"
+         },
+         {
+            title : "GreenSpace: Park bench will monitor air pollution",
+            author : "Sandy Bauers",
+            source : "Philly.Com",
+            date : new Date("2015-04-26 00:00:00"),
+            url : "http://www.philly.com/philly/health/20150426_GreenSpace__Park_bench_will_monitor_air_pollution.html"
+         },
+         {
+            title : "Infosys Innovation Fund Invests in Air Quality Monitoring Pioneer, Airviz",
+            author : "",
+            source : "Infosys Press Release",
+            date : new Date("2015-04-24 00:00:00"),
+            file : {
+               name : "20150424_Infosys.pdf",
+               type : "pdf"
+            },
+            url : "http://www.infosys.com/newsroom/press-releases/Pages/air-quality-monitoring-pioneer.aspx"
+         },
+         {
+            title : "KOMO4 News: Happy Earth Day 2015",
+            author : "",
+            source : "KOMO4 News",
+            date : new Date("2015-04-22 00:00:00"),
+            url : "https://www.youtube.com/watch?v=o6oCa9LiX1I&t=164"
+         },
+         {
+            title : "Essential Pittsburgh: Testing Indoor Air Quality",
+            author : "",
+            source : "WESA",
+            date : new Date("2015-04-21 00:00:00"),
+            file : {
+               name : "20150421_WESA.pdf",
+               type : "pdf"
+            },
+            url : "http://wesa.fm/post/essential-pittsburgh-rising-amount-radon-pennsylvania-air"
+         },
+         {
+            title : "Experimenting at Home With Air Quality Monitors",
+            author : "Kate Galbraith",
+            source : "The New York Times",
+            date : new Date("2015-04-15 00:00:00"),
+            file : {
+               name : "20150415_New_York_Times.pdf",
+               type : "pdf"
+            },
+            url : "http://www.nytimes.com/2015/04/16/business/experimenting-at-home-with-air-quality-monitors.html"
+         },
+         {
+            title : "Urban Defence: Tracking Tools",
+            author : "Madeleine Cuff and Sam Gaskin",
+            source : "Stylus",
+            date : new Date("2015-04-13 00:00:00"),
+            file : {
+               name : "20150413_Stylus.pdf",
+               type : "pdf"
+            },
+            url : ""
+         },
+         {
+            title : "Carnegie Library's take-out air-quality monitors are a hit with patrons",
+            author : "Ashley Murray",
+            source : "Pittsburgh City Paper",
+            date : new Date("2015-04-08 00:00:00"),
+            file : {
+               name : "20150408_CityPaper.pdf",
+               type : "pdf"
+            },
+            url : "http://www.pghcitypaper.com/pittsburgh/carnegie-librarys-take-out-air-quality-monitors-are-a-hit-with-patrons/Content?oid=1817043"
+         },
+         {
+            title : "Speck, an in-home air monitor that assesses air quality and counts fugitive dust",
+            author : "Deb Smit",
+            source : "Next Pittsburgh",
+            date : new Date("2015-03-30 00:00:00"),
+            file : {
+               name : "20150330_Next_Pittsburgh.pdf",
+               type : "pdf"
+            },
+            url : "http://www.nextpittsburgh.com/business-tech-news/speck-home-air-monitor-assesses-air-quality-counts-fugitive-dust/"
+         },
+         {
+            title : "CMU Tech Company Aims to Empower Pittsburgh with Cleaner Air",
+            author : "Jessica Nath",
+            source : "WESA",
+            date : new Date("2015-03-30 00:00:00"),
+            file : {
+               name : "20150330_WESA.pdf",
+               type : "pdf"
+            },
+            url : "http://wesa.fm/post/cmu-tech-company-aims-empower-pittsburgh-cleaner-air"
+         },
+         {
+            title : "Carnegie Mellon University's Speck device monitors indoor pollution",
+            author : "Aaron Aupperlee",
+            source : "Pittsburgh Tribune Review",
+            date : new Date("2015-03-29 00:00:00"),
+            file : {
+               name : "20150329_Pittsburgh_Tribune_Review.pdf",
+               type : "pdf"
+            },
+            url : "http://triblive.com/news/allegheny/8008660-74/speck-particles-quality#axzz3aSa7EzTz"
+         },
+         {
+            title : "Introducing Speck. The New Home Air Quality Monitor",
+            author : "",
+            source : "Environmental Technology Online",
+            date : new Date("2015-03-25 00:00:00"),
+            file : {
+               name : "20150325_Environmental_Technology_Online.pdf",
+               type : "pdf"
+            },
+            url : "http://www.envirotech-online.com/news/air-monitoring/6/breaking_news/introducing_speck.the_new_home_air_quality_monitor/33949/"
+         },
+         {
+            title : "Speck monitors your home's air quality",
+            author : "Antonio Pasolini",
+            source : "gizmag",
+            date : new Date("2015-03-18 00:00:00"),
+            url : "http://www.gizmag.com/speck-indoor-air-monitor/36575/"
+         },
+         {
+            title : "Carnegie Mellon Spinoff Introduces Speck, a Personal, Wi-Fi-connected Air Quality Monitor",
+            author : "Byron Spice",
+            source : "CMU",
+            date : new Date("2015-03-16 00:00:00"),
+            file : {
+               name : "20150316_CMU_News.pdf",
+               type : "pdf"
+            },
+            url : "http://www.cmu.edu/news/stories/archives/2015/march/speck-air-quality-monitor.html"
+         },
+         {
+            title : "Air monitors provided to residents near Marcellus sites",
+            author : "Emily Petsko",
+            source : "Observer-Reporter",
+            date : new Date("2014-07-19 00:00:00"),
+            file : {
+               name : "20140719_Observer_Reporter.pdf",
+               type : "pdf"
+            },
+            url : "http://www.observer-reporter.com/article/20140719/NEWS080101/140719418"
+         }
+      ]
+   });
 });
 
 router.get('/jobs', function(req, res) {
